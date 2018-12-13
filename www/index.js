962a2d06b3d2767b8ff7cc9ed9e2f61d11e22593
Jordy Moos <jordy.moos@ing.com>
2018-12-13 16:01:03 +0100

    Working on Game of Life

--- www/index.js
+++ www/index.js
@@ -1,3 +1,6 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet();
+const pre = document.getElementById('game-of-life-canvas');
+const universe = Universe.new();
+
+
